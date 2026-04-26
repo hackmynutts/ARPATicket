7ba68946e35dec43ee22e5bae3bf2f0b355e9e38
hackmynutts <jorgemoruagonzalez@gmail.com>
2026-04-26 17:48:23 -0600

    starting template pattern

---
+++ ARPATicket.API/Services/BaseEntityService.cs
@@ -0,0 +1,21 @@
+﻿namespace ARPATicket.API.Services
+{
+    public abstract class BaseEntityService<TAddDTO, TModel, TResultDTO>
+    {
+        //definimos el template
+        //define el orden de los pasos a seguir para crear una entidad, pero deja la implementación de cada paso a las clases derivadas
+
+        public async Task<TResultDTO> AddAsync(TAddDTO dto) //es lo mismo que TicketAddDTO, pero lo hacemos genérico para que pueda ser reutilizado por otras entidades
+        {
+            var externalData = await GetExternalDataAsync(dto);
+            var model = MapToModel(dto, externalData);
+            var result = await SaveAsync(model);
+            return MapToResultDTO(result);
+        }
+        //procedimientos que los hijos implementarán, cada entidad tendrá su propia forma de obtener datos externos, mapear a modelo, guardar y mapear a DTO de resultado
+        protected abstract Task<object> GetExternalDataAsync(TAddDTO dto);
+        protected abstract TModel MapToModel(TAddDTO dto, object externalData);
+        protected abstract Task<TModel> SaveAsync(TModel model);
+        protected abstract TResultDTO MapToResultDTO(TModel model);
+    }
+}
